32edbb7d62b2a2c5755896dd254f81a20d843cd1
Pavan Kusuma <pavankusuma@Pavans-MacBook-Pro.local>
2024-01-05 09:10:49 +0530

    Chat API 1

--- app/api/psych/chat/[...ids]/route.js
+++ app/api/psych/chat/[...ids]/route.js
@@ -6,6 +6,11 @@ const OneSignal = require('onesignal-node')
 
 const client = new OneSignal.Client(process.env.ONE_SIGNAL_APPID, process.env.ONE_SIGNAL_APIKEY)
 
+// S1 –––– New chat
+// S2 –––– Assign admin to chat (Admin)
+// S3 –––– Get chat (Student)
+// S4 –––– Get chats (Admin)
+
 // create new chat and updates existing chat for a user
 export async function GET(request,{params}) {
 
@@ -13,7 +18,8 @@ export async function GET(request,{params}) {
     const connection = await pool.getConnection();
 
     // current date time for updating
-    var currentDate =  dayjs(new Date(Date.now())).format('YYYY-MM-DD HH:mm:ss');
+    var sentAt =  dayjs(new Date(Date.now())).format('YYYY-MM-DD HH:mm:ss');
+    var currentDate =  dayjs(new Date(Date.now())).format('YYYY-MM-DD');
 
     try{
 
@@ -21,56 +27,74 @@ export async function GET(request,{params}) {
         if(await Keyverify(params.ids[0])){
 
                 try {
+                  if(params.ids[1] == 'S1'){
 
-                    // check if any active request exists for the provided collegeId
-                    const q0 = 'select collegeId from psych_appointment where collegeId="'+params.ids[4]+'" and isOpen = 1';
-                    const [rows0, fields0] = await connection.execute(q0);
-                    
-                    if(rows0.length == 0){
+                      // create query for insert
+                      const q = 'INSERT INTO psych_chat (collegeId, adminId, message, sentAt, chatDate, campusId) VALUES ( ?, ?, ?, ?, ?, ?)';
+                      // create new message
+                      const [rows, fields] = await connection.execute(q, [ params.ids[2], params.ids[3], decodeURIComponent(params.ids[4]), sentAt, currentDate, params.ids[5]]);
+                      connection.release();
 
-                        // create query for insert
-                        const q = 'INSERT INTO psych_appointment (collegeId, description, requestDate, isOpen, requestStatus) VALUES ( ?, ?, ?, ?, ?)';
-                        // create new request
-                        const [rows, fields] = await connection.execute(q, [ params.ids[1], decodeURIComponent(params.ids[2]), currentDate, 1,  "Submitted"]);
-                        connection.release();
-
-                        // send SMS to parent
-                        // sendSMS(params.ids[11], params.ids[12], dayjs(params.ids[6]).format('DD-MM-YY hh:mm A'), dayjs(params.ids[7]).format('DD-MM-YY hh:mm A'))
-                        // sendSMS(params.ids[11], params.ids[12], dayjs(params.ids[6]).format('DD-MM-YY hh:mm A'), dayjs(params.ids[7]).format('YYYY-MM-DD'))
-
-                        // get the gcm_regIds of SuperAdmin and branch admin to notify
-                        const [nrows, nfields] = await connection.execute('SELECT gcm_regId FROM `user` where role IN ("SuperAdmin_P")');
-
-                        // get the gcm_regIds list from the query result
-                        var gcmIds = [];
-                        for (let index = 0; index < nrows.length; index++) {
-                          const element = nrows[index].gcm_regId;
-                          if(element.length > 3)
-                            gcmIds.push(element); 
-                        }
-
-                        // var gcmIds = 
-                        // console.log(gcmIds);
-
-                        // send the notification
-                        const notificationResult = await send_notification('New appointment request received!', gcmIds, 'Multiple');
-                            
-                        // return successful update
-                        return Response.json({status: 200, message:'Request submitted!', notification: notificationResult}, {status: 200})
-
-                        // return the user data
-                        // return Response.json({status: 200, message:'Request submitted!'}, {status: 200})                   
+                      // return successful update
+                      return Response.json({status: 200, message:'Message sent!'}, {status: 200})                 
+                  }
+                  else if(params.ids[1] == 'S2'){
+                      // Assign admin to user
+                      // const query1 = `UPDATE psych_chat SET adminId = ? where collegeId=?`;
+                      const query1 = `UPDATE psych_chat
+                                        SET adminId = CASE
+                                            WHEN adminId IS '-' THEN ?
+                                            ELSE CONCAT(adminId, ',', ?)
+                                        END
+                                        WHERE collegeId = ?;`;
                       
-                      
-                             
-                      
+                      const [rows1, fields1] = await connection.execute(query1, [params.ids[2], params.ids[2], params.ids[3]]);
+                      connection.release();
 
-                    }
-                    else {
-                      // return message to close active requests
-                      return Response.json({status: 201, message:'Close active requests before raising new one!'}, {status: 201})
-                    }
+                      if(rows1.length > 0){
+                          // return the messages data
+                          return Response.json({status: 200, message:'Admin assigned!', data: rows1}, {status: 200})
 
+                      }
+                      else {
+                          // user doesn't exist in the system
+                          return Response.json({status: 404, message:'No more messages!'}, {status: 200})
+                      }
+                  }
+                  else if(params.ids[1] == 'S3'){
+                      // get the messages of user
+                      const query1 = `select message,sentAt,chatDate from psych_chat where collegeId=? ORDER BY sentAt DESC LIMIT 20 OFFSET ?`;
+                      
+                      const [rows1, fields1] = await connection.execute(query1, [params.ids[2], params.ids[3]]);
+                      connection.release();
+
+                      if(rows1.length > 0){
+                          // return the messages data
+                          return Response.json({status: 200, message:'Data found!', data: rows1}, {status: 200})
+
+                      }
+                      else {
+                          // user doesn't exist in the system
+                          return Response.json({status: 404, message:'No more messages!'}, {status: 200})
+                      }
+                  }
+                  else if(params.ids[1] == 'S4'){
+                      // get the messages of user by admin
+                      const query1 = `select collegeId,MAX(sentAt) as sentAt from psych_chat where adminId LIKE ? GROUP BY collegeId ORDER BY sentAt DESC LIMIT 20 OFFSET ?`;
+                                            
+                      const [rows1, fields1] = await connection.execute(query1, ["%"+params.ids[2]+"%", params.ids[3]]);
+                      connection.release();
+
+                      if(rows1.length > 0){
+                          // return the messages data
+                          return Response.json({status: 200, message:'Data found!', data: rows1}, {status: 200})
+
+                      }
+                      else {
+                          // user doesn't exist in the system
+                          return Response.json({status: 404, message:'No more messages!'}, {status: 200})
+                      }
+                  }
                     
                 } catch (error) {
                     // user doesn't exist in the system
@@ -91,19 +115,6 @@ export async function GET(request,{params}) {
     
   }
 
-  // function to call the SMS API
-  async function sendSMS(name, number, from, to){
-
-    const result  = await fetch("http://webprossms.webprosindia.com/submitsms.jsp?user=SVCEWB&key=c280f55d6bXX&mobile="+number+"&message=Dear Parent, your ward, "+name+" has applied for outing from "+from+" to "+to+".  SVECWB Hostels&senderid=SVECWB&accusage=1&entityid=1001168809218467265&tempid=1007149047352803219", {
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json",
-              Accept: "application/json",
-          },
-        });
-          const queryResult = await result.text() // get data
-        //   console.log(queryResult);
-  }
   
 
   // send the notification using onesignal.
